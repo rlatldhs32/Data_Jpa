cae20f7e8d1dc97276e135f6ae85cb4471965208
rlatldhs32 <rlatldhs32@naver.com>
2022-02-23 23:53:05 +0900

    220223 Update

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,19 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+}
